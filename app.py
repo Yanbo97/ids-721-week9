881e4c9d9635cd333c42b8c6ae17fa213331bf26
Yanbo <yg229@duke.edu>
2024-04-06 00:00:07 -0400

    test

--- app.py
+++ app.py
@@ -1,12 +1,15 @@
-
 from transformers import pipeline
 import streamlit as st
 
-model = pipeline("ner", model="dbmdz/bert-large-cased-finetuned-conll03-english", grouped_entities=True)
+model = pipeline("ner", model="dbmdz/bert-large-cased-finetuned-conll03-english", use_pytorch=True, aggregation_strategy="simple")
+
 input_text = st.text_area("Enter Text", height=100)
 generate_button = st.button("Identify Entities")
-if generate_button:
-    if input_text:
-        results = model(input_text)
-        for result in results:
-            st.markdown(f"**Entity**: {result['word']}, **Type**: {result['entity_group']}, **Score**: {result['score']:.4f}")
+
+if generate_button and input_text:
+    results = model(input_text)
+    for result in results:
+
+        entity = f"Entity: {result['entity_group']} (Score: {result['score']:.4f})"
+        text = f"Text: {result['word']}"
+        st.markdown(f"**{entity}** - {text}")
