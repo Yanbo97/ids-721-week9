a7ef4019a37d6d1b914a794cd3382a1fc8815945
Yanbo <yg229@duke.edu>
2024-04-06 00:13:25 -0400

    test

--- app.py
+++ app.py
@@ -1,10 +1,11 @@
 from transformers import pipeline
 import streamlit as st
 
-model = pipeline("summarization")
+model_name = "sshleifer/distilbart-cnn-12-6"
+model = pipeline("summarization", model=model_name)
+
 input_text = st.text_area("Enter Text", height=100)
 generate_button = st.button("Summarize Text")
-if generate_button:
-    if input_text:
-        output_text = model(input_text, max_length=130, min_length=30, do_sample=False)[0]["summary_text"]
-        st.markdown(f"**Summary**: {output_text}")
+if generate_button and input_text:
+    output_text = model(input_text, max_length=130, min_length=30, do_sample=False)[0]["summary_text"]
+    st.markdown(f"**Summary**: {output_text}")
