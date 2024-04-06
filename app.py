e80e107e0e9c9cae515208d68cefba9f745eb175
Yanbo <yg229@duke.edu>
2024-04-06 00:04:05 -0400

    change

--- app.py
+++ app.py
@@ -1,15 +1,10 @@
 from transformers import pipeline
 import streamlit as st
 
-model = pipeline("ner", model="dbmdz/bert-large-cased-finetuned-conll03-english", use_pytorch=True, aggregation_strategy="simple")
-
+model = pipeline("summarization")
 input_text = st.text_area("Enter Text", height=100)
-generate_button = st.button("Identify Entities")
-
-if generate_button and input_text:
-    results = model(input_text)
-    for result in results:
-
-        entity = f"Entity: {result['entity_group']} (Score: {result['score']:.4f})"
-        text = f"Text: {result['word']}"
-        st.markdown(f"**{entity}** - {text}")
+generate_button = st.button("Summarize Text")
+if generate_button:
+    if input_text:
+        output_text = model(input_text, max_length=130, min_length=30, do_sample=False)[0]["summary_text"]
+        st.markdown(f"**Summary**: {output_text}")
