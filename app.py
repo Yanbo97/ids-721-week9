5af48ab5bbe8d8d0d7b1d8f583e938ccf8baf0b6
Yanbo <yg229@duke.edu>
2024-04-05 23:44:01 -0400

    first

--- app.py
+++ app.py
@@ -1,1 +1,12 @@
 
+from transformers import pipeline
+import streamlit as st
+
+model = pipeline("ner", grouped_entities=True)
+input_text = st.text_area("Enter Text", height=100)
+generate_button = st.button("Identify Entities")
+if generate_button:
+    if input_text:
+        results = model(input_text)
+        for result in results:
+            st.markdown(f"**Entity**: {result['word']}, **Type**: {result['entity_group']}, **Score**: {result['score']:.4f}")
